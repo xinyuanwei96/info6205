ce348210ee4aada688d02b8e6e8e42c5e35edd8d
weixinyuan11111 <xinyuanwei1996@gmail.com>
2019-12-03 23:09:00 -0500

    HeapSort

---
+++ INFO6205-master/src/main/java/edu/com/coe/info6205/heapsort/HeapSort.java
@@ -0,0 +1,76 @@
+package edu.com.coe.info6205.heapsort;
+
+public class HeapSort<X extends Comparable<X>> implements Sort<X> {
+
+    /**
+     * Constructor for InsertionSort
+     *
+     * @param helper an explicit instance of Helper to be used.
+     */
+    public HeapSort(Helper<X> helper) {
+        this.helper = helper;
+    }
+
+    public HeapSort() {
+        this(new Helper<>("HeapSort"));
+    }
+
+    @Override
+    public void sort(X[] array, int from, int to) {
+        if (array == null || array.length <= 1) {
+				return;
+			}
+ 
+			buildMaxHeap(array);
+ 
+			for (int i = array.length - 1; i >= 1; i--) {
+				
+                                 X temp = array[0];
+				array[0] = array[i];
+				array[i] = temp;
+				maxHeap(array, i, 0);
+			}
+    }
+    private  void buildMaxHeap(X[] array) {
+			if (array == null || array.length <= 1) {
+				return;
+			}
+ 
+			int half = array.length / 2;
+			for (int i = half; i >= 0; i--) {
+				maxHeap(array, array.length, i);
+			}
+		}
+ 
+		private  void maxHeap(X[] array, int heapSize, int index) {
+			int left = index * 2 + 1;
+			int right = index * 2 + 2;
+ 
+			int largest = index;
+			if (left < heapSize && helper.compare(array[index],array[left])<0) {
+				largest = left;
+			}
+ 
+			if (right < heapSize && helper.compare(array[largest],array[right])<0 ) {
+				largest = right;
+			}
+ 
+			if (index != largest) {
+				helper.swap(array, index, largest);
+ 
+				maxHeap(array, heapSize, largest);
+			}
+		}
+
+    @Override
+    public String toString() {
+        return helper.toString();
+    }
+
+    @Override
+    public Helper<X> getHelper() {
+        return helper;
+    }
+
+    private final Helper<X> helper;
+}
